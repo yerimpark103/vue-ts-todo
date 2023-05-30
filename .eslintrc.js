bf6e49d686af4d5c9efabfe4c3c3c39a0a51993d
claud_park <yerimpark103@gmail.com>
2023-05-31 01:22:35 +0900

    Create todo app with input binding

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,29 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto', // 한줄 추가
+      },
+    ],
   },
 };
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input id="todo-input" type="text" :value="item" @input="handleInput" />
+    <button @click="addTodo" type="button">Add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: ['item'],
+  methods: {
+    handleInput(event: any) {
+      this.$emit('input', event.target.value);
+    },
+    addTodo() {
+      console.log('add');
+    },
+  },
+});
+</script>
+
+<style scoped></style>
