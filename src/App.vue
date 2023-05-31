0b7ea85ccecab462a521f7c4e8033292448d8603
claud_park <yerimpark103@gmail.com>
2023-05-31 17:20:30 +0900

    Remove todo items based on index

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,9 @@
           <TodoListItem
             v-for="(todoItem, index) in todoItems"
             :key="index"
+            :index="index"
             :todoItem="todoItem"
+            @remove="removeTodoItem"
           />
         </ul>
       </div>
@@ -61,6 +63,10 @@ export default Vue.extend({
     },
     fetchTodoItems() {
       this.todoItems = storage.fetch();
+    },
+    removeTodoItem(index: number) {
+      this.todoItems.splice(index, 1);
+      storage.save(this.todoItems);
     },
   },
 
--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <li>{{ todoItem }}</li>
+    <li>
+      <span>
+        {{ todoItem }}
+      </span>
+      <button @click="removeItem">삭제</button>
+    </li>
   </div>
 </template>
 
@@ -10,6 +15,13 @@ import Vue from 'vue';
 export default Vue.extend({
   props: {
     todoItem: String,
+    index: Number,
+  },
+
+  methods: {
+    removeItem() {
+      this.$emit('remove', this.index);
+    },
   },
 });
 </script>
